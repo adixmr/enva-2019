4300dc40a9612952217c88a74822a81639849f02
adixmr <42894359+adixmr@users.noreply.github.com>
2018-12-20 02:04:57 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,254 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="Colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Event Asia</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">=
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link href="css/availability-calendar.css" rel="stylesheet">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>
+
+			<!-- Start Header Area -->
+			<header class="default-header">
+				<div class="container">
+					<div class="header-wrap">
+						<div class="header-top d-flex justify-content-between align-items-center">
+							<div class="logo">
+								<a href="#home"><img src="img/logo.png" alt=""></a>
+							</div>
+						</div>	
+					</div>
+				</div>
+			</header>
+			<!-- End Header Area -->
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+						<div class="row fullscreen align-items-center justify-content-center" style="height: 775px;">
+							<div class="banner-content col-lg-6 col-md-12">
+								<h1><strong>Enva Starts In</strong></h1>
+				        		<div class="row clock_sec d-flex flex-row justify-content-between" id="clockdiv">
+				                    <div class="clockinner">
+				                        <span class="days"></span>
+				                        <div class="smalltext">Days</div>
+				                    </div>
+				                    <div class="clockinner">
+				                        <span class="hours"></span>
+				                        <div class="smalltext">Hours</div>
+				                    </div>
+				                    <div class="clockinner">
+				                        <span class="minutes"></span>
+				                        <div class="smalltext">Minutes</div>
+				                    </div>
+				                    <div class="clockinner">
+				                        <span class="seconds"></span>
+				                        <div class="smalltext">Seconds</div>
+				                    </div>
+
+				                </div>
+							</div>
+						</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+			<!-- Start facilities Area -->
+			<section class="facilities-area section-gap" id="functionalities" tabindex="-1">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="col-md-8 pb-80 header-text">
+							<h1>Events</h1>
+							<p>Lorem ipsum dolor sit amet</p>
+						</div>
+					</div>
+					<div class="row justify-content-center">
+						<div class="col-centered col-lg-4 col-md-6 pb-40">
+							<div class="card">
+							  <img class="card-img-top" src="http://placehold.jp/286x180.png" alt="Card image cap">
+							  <div class="card-body">
+								<center><h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Know More!</a>
+							  </div>
+							</div>
+							</div>
+													<div class="col-lg-4 col-md-6 pb-40">
+							<div class="card">
+							  <img class="card-img-top" src="http://placehold.jp/286x180.png" alt="Card image cap">
+							  <div class="card-body">
+								<center><h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Know More!</a>
+							  </div>
+							</div>
+							</div>
+													<div class="col-lg-4 col-md-6 pb-40">
+							<div class="card">
+							  <img class="card-img-top" src="http://placehold.jp/286x180.png" alt="Card image cap">
+							  <div class="card-body">
+								<center><h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Know More!</a>
+							  </div>
+							</div>
+							</div>
+													<div class="col-lg-4 col-md-6 pb-40">
+							<div class="card">
+							  <img class="card-img-top" src="http://placehold.jp/286x180.png" alt="Card image cap">
+							  <div class="card-body">
+								<center><h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Know More!</a>
+							  </div>
+							</div>
+							</div>
+													<div class="col-lg-4 col-md-6 pb-40">
+							<div class="card">
+							  <img class="card-img-top" src="http://placehold.jp/286x180.png" alt="Card image cap">
+							  <div class="card-body">
+								<center><h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Know More!</a>
+							  </div>
+							</div>
+							</div>
+													<div class="col-lg-4 col-md-6 pb-40">
+							<div class="card">
+							  <img class="card-img-top" src="http://placehold.jp/286x180.png" alt="Card image cap">
+							  <div class="card-body">
+								<center><h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a href="#" class="btn btn-primary">Know More!</a>
+							  </div>
+							</div>
+							</div>
+
+
+					</div>
+				</div>
+			</section>
+			<!-- End facilities Area -->
+
+			<!-- Start speaker Area -->
+			<section class="speaker-area section-gap" id="speaker" tabindex="-1">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="col-md-8 pb-80 header-text">
+							<h1>Our Speakers / Meet Our Team / Some Other Shit?</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> labore  et dolore magna aliqua.
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-4 col-md-4 speaker-wrap">
+							<div class="single-speaker">
+								<div class="content">
+								    <a href="#" target="_blank">
+								      <div class="content-overlay"></div>
+								  		 <img class="content-image img-fluid d-block mx-auto" src="img/s1.jpg" alt="">
+								      	<div class="content-details fadeIn-bottom"></div>
+								    </a>
+								 </div>
+							</div>
+						  	<h2>Launda 1</h2>
+						  	<p>inappropriate behavior</p>
+						</div>
+						<div class="col-lg-4 col-md-4 speaker-wrap">
+							<div class="single-speaker">
+								<div class="content">
+								    <a href="#" target="_blank">
+								      <div class="content-overlay"></div>
+								  		 <img class="content-image img-fluid d-block mx-auto" src="img/s2.jpg" alt="">
+								      	<div class="content-details fadeIn-bottom"></div>
+								    </a>
+								 </div>
+							</div>
+						  	<h2>Launda 2</h2>
+						  	<p>inappropriate behavior</p>
+						</div>
+						<div class="col-lg-4 col-md-4 speaker-wrap">
+							<div class="single-speaker">
+								<div class="content">
+								    <a href="#" target="_blank">
+								      <div class="content-overlay"></div>
+								  		 <img class="content-image img-fluid d-block mx-auto" src="img/s3.jpg" alt="">
+								      	<div class="content-details fadeIn-bottom"></div>
+								    </a>
+								 </div>
+							</div>
+						  	<h2>Laundiya 1? xD</h2>
+						  	<p>appropriate behavior</p>
+						</div>
+
+					</div>
+				</div>
+			</section>
+			<!-- End speaker Area -->
+
+
+			
+
+			<!-- start footer Area -->
+			<footer class="footer-area section-gap" style="padding-top: 0; padding-bottom:60px;">
+				<div class="container">
+					
+
+					<div class="row footer-bottom d-flex justify-content-between">
+					
+						<div class="col-lg-8 col-sm-12 footer-text m-0 text-white">
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							<center><p>Copyright © <script>document.write(new Date().getFullYear());</script> | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank">Team Prakriti</a></p></center>
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						</div>
+						</center>
+						<div class="col-lg-4 col-sm-12 footer-social">
+						<center>
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</center>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/availability-calendar.js"></script>
+			<script src="js/jquery.sticky.js"></script>
+			<script src="js/parallax.min.js"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+			<script src="js/countdown.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>
+			<script src="js/main.js"></script>
+		</body>
+	</html>
