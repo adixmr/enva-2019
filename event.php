87dcdd13aa6b01ac4f124a2cbb9c2cb4795eb9f5
adixmr <42894359+adixmr@users.noreply.github.com>
2018-12-20 02:05:24 +0530

    Create event.php

---
+++ event.php
@@ -0,0 +1,238 @@
+<html lang="zxx" class="no-js"><head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="Colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Event Asia</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <style>
+        .bs-callout {
+    padding: 20px;
+    margin: 10px 0;
+    border: 1px solid #eee;
+    border-left-width: 5px;
+    border-radius: 3px;
+}
+.bs-callout h4 {
+    margin-top: 0;
+    margin-bottom: 5px;
+}
+.bs-callout p:last-child {
+    margin-bottom: 0;
+}
+.bs-callout code {
+    border-radius: 3px;
+}
+.bs-callout+.bs-callout {
+    margin-top: -5px;
+}
+.bs-callout-default {
+    border-left-color: #777;
+}
+.bs-callout-default h4 {
+    color: #777;
+}
+.bs-callout-primary {
+    border-left-color: #428bca;
+}
+.bs-callout-primary h4 {
+    color: #428bca;
+}
+.bs-callout-success {
+    border-left-color: #5cb85c;
+}
+.bs-callout-success h4 {
+    color: #5cb85c;
+}
+.bs-callout-danger {
+    border-left-color: #d9534f;
+}
+.bs-callout-danger h4 {
+    color: #d9534f;
+}
+.bs-callout-warning {
+    border-left-color: #f0ad4e;
+}
+.bs-callout-warning h4 {
+    color: #f0ad4e;
+}
+.bs-callout-info {
+    border-left-color: #5bc0de;
+}
+.bs-callout-info h4 {
+    color: #5bc0de;
+}
+        </style>
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css"></head><body>=
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link href="css/availability-calendar.css" rel="stylesheet">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/main.css">
+		
+		
+
+			<!-- Start Header Area -->
+			<div id="undefined-sticky-wrapper" class="sticky-wrapper" style="height: 70px;"><header class="default-header" style="">
+				<div class="container">
+					<div class="header-wrap">
+						<div class="header-top d-flex justify-content-between align-items-center">
+							<div class="logo">
+								<a href="#home"><img src="img/logo.png" alt=""></a>
+							</div>
+						</div>	
+					</div>
+				</div>
+			</header></div>
+			<!-- End Header Area -->
+
+			<!-- start banner Area -->
+			
+			<!-- End banner Area -->
+
+			<!-- Start facilities Area -->
+			
+			<!-- End facilities Area -->
+
+			<!-- Start speaker Area -->
+			<section class="speaker-area section-gap" id="speaker" tabindex="-1" style="padding-top: 30px; background-color: white;">
+				<div class="container jumbotron">
+					<div class="row d-flex justify-content-center">
+						<div class="col-md-8 pb-80 header-text" style="padding-bottom: 20px;">
+							<h1>The event title goes here</h1>
+							<p><strong>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut  labore  et dolore magna aliqua.
+							<strong></strong></strong></p>
+						</div>
+					</div>
+					<div class="bd-example">
+<ul class="list-group">
+  <li class="list-group-item active">Rounds description</li>
+  
+
+<div class="bs-callout bs-callout-primary" style="background-color: white;">
+  <h4>Primary Callout</h4>
+  This is a primary callout.
+</div>
+
+<div class="bs-callout bs-callout-primary" style="background-color: white;">
+  <h4>Primary Callout</h4>
+  This is a primary callout.
+</div>
+
+<div class="bs-callout bs-callout-primary" style="background-color: white;">
+  <h4>Primary 
+      
+      Callout</h4>
+  This is a primary callout.
+</div><div class="bs-callout bs-callout-primary" style="background-color: white;">
+  <h4>Primary Callout</h4>
+  This is a primary callout.
+</div></ul></div><br>
+
+
+
+<div class="bd-example">
+<ul class="list-group">
+  <li class="list-group-item active">Team Details</li>
+  <li class="list-group-item">LMAO</li>
+</ul>
+</div>
+
+<br>
+
+
+<div class="bd-example">
+<ul class="list-group">
+  <li class="list-group-item active">Registration form</li>
+<li class="list-group-item">
+    
+    
+        <div class="input-group mb-3">
+      <span class="input-group-addon" id="basic-addon1">Name</span>
+
+  <input type="text" class="form-control" placeholder="John Doe" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+    
+        <div class="input-group mb-3">
+      <span class="input-group-addon" id="basic-addon1">Email</span>
+
+  <input type="text" class="form-control" placeholder="myname@domain.tld" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+    
+        <div class="input-group mb-3">
+      <span class="input-group-addon" id="basic-addon1">Phone</span>
+
+  <input type="text" class="form-control" placeholder="9785634120" aria-label="Username" aria-describedby="basic-addon1">
+</div><div class="input-group mb-3">
+      <span class="input-group-addon" id="basic-addon1">Name</span>
+
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+    </li>
+    </ul></div>
+
+
+
+
+				</div>
+			</section>
+			<!-- End speaker Area -->
+
+
+			
+
+			<!-- start footer Area -->
+			<footer class="footer-area section-gap" style="padding-top: 0; padding-bottom:60px;">
+				<div class="container">
+					
+
+					<div class="row footer-bottom d-flex justify-content-between">
+					
+						<div class="col-lg-8 col-sm-12 footer-text m-0 text-white">
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							<center><p>Copyright © <script>document.write(new Date().getFullYear());</script>| Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank">Team Prakriti</a></p></center>
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						</div>
+						
+						<div class="col-lg-4 col-sm-12 footer-social">
+						<center>
+							<a href="#"><i class="fa fa-facebook"></i></a>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+							<a href="#"><i class="fa fa-behance"></i></a>
+						</center>
+						</div>
+					</div>
+				</div>
+			</footer>
+			<!-- End footer Area -->
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+			<script src="js/vendor/bootstrap.min.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/availability-calendar.js"></script>
+			<script src="js/jquery.sticky.js"></script>
+			<script src="js/parallax.min.js"></script>
+			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+			<script src="js/countdown.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>
+			<script src="js/main.js"></script>
+		
+	
+</body></html>
